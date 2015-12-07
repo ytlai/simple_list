df339b63f51b4867e9c3d2184d05cb7514396d13
ytlai <ytlai@fortinet.com>
2015-12-07 10:44:17 +0800

    add interface and macro-like function

--- simple_list.h
+++ simple_list.h
@@ -3,9 +3,8 @@
 #include <string.h>
 #include "list.h"
 
-
 enum{
-	Input,
+	Add,
 	Delete,
 	List,
 	Quit
@@ -13,25 +12,90 @@ enum{
 
 enum{
 	TAIL,
-	HEAD
+	HEAD,
+	NONE
 };
 
-typedef struct num_list{
-	int num;
+typedef struct area{
 	int index;
-	struct list_head my_list;
+	int num;
+	struct list_head list;
+}area;
 
-}numl;
+/*item could be head or element*/
+typedef struct double_linked_list{
+	area *item;
+}d_linkd_list;
 
-/*item could be head or member*/
-typedef struct multi_lists{
-	numl *item;
+#define NEW_D_LINKED_LIST(n, s_t, s_p,t) do{ \
+		area *root; \
+		root = area_new(0,n); \
+		INIT_LIST_HEAD(&(root->list)); \
+		j=0; \
+		for(j=s_t;j<s_p;j++) \
+			add_element (root,j,elements,ele_num,ele_space); \
+		if (t<2) { \
+			do_merge(t,((head_list+i)->item),root); \
+			free(root); \
+		} \
+		else { \
+			free(head_list[i].item); \
+			head_list[i].item = root; \
+		} \
+		} while(0)
 
-}multil;
+#define DEL_AREA(n,s_t) do {\
+		j=k=f=0; \
+		k = area_count((head_list+n)->item); \
+		while(1){ \
+			list_for_each_entry(tmp,&(((head_list+n)->item)->list),list){ \
+				++j; \
+				if (tmp->num >=s_t && tmp->num<=stop){ \
+					f=1; \
+					break; \
+				} \
+			} \
+			if (j==k && f==0) \
+				break; \
+			if (f) { \
+				list_del(&(tmp->list)); \
+				_del_element(elements,tmp->index,ele_num); \
+				f=0; \
+			} \
+			j=k=0; \
+			k = area_count((head_list+n)->item); \
+		} \
+} while(0)
 
-int add_section(multil **mlr_ptr, multil **all, int start, int stop, int *len_of_mlr, int *all_len, int *s_mlr, int*s_all);
-int del_section(multil **mlr_ptr, multil **all, int start, int stop, int *len_of_mlr, int *all_len, int *s_mlr, int *s_all);
+
+#define FIND_BEST_AREA() do { \
+	int *matched_num = (int*) calloc (*head_num,sizeof(int)); \
+	for (i=0;i<*head_num;i++) { \
+		_area = ((head_list+i)->item); \
+		if (!area_find_range(_area,start,stop)) \
+			return 0; \
+		else { \
+			for (j=start;j<stop+1;j++) \
+				if (!area_find(_area,j)) \
+					++matched_num[i]; \
+		} \
+	} \
+	for (i=0;i<*head_num;i++) \
+		if (matched_num[i] > max_match){ \
+			best_idx = i; \
+			max_match = matched_num[i]; \
+		} \
+	free (matched_num); \
+}while(0)
+
+area *area_new (int index, int num);
+int area_insert(d_linkd_list **heads, d_linkd_list **elements, int start, int stop, int *head_num, int *ele_num, int *head_space, int *ele_space);
+int area_delete(d_linkd_list **heads, d_linkd_list **elements, int start, int stop, int *head_num, int *ele_num, int *head_space, int *ele_space);
+int area_find(area *area, int num);
+int area_find_range(area *area, int start, int stop);
+int area_count(area *_area);
+void area_dump(area *_area);
+void show_all_areas(d_linkd_list* head_list, int head_num);
 int input_handler(char input[10],int *op);
-int init(multil **mlr_ptr,multil **all, int *all_len,int *s_all);
-void show_all_lists(multil* mlr, int len_of_mlr);
+
 
